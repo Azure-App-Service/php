2950af4807f6ac27b75e117695feef8652a7b6c4
Nazim Lala <naziml@gmail.com>
2017-01-18 15:24:39 -0800

    Create init_container.sh

---
+++ 7.0.6-apache-0/init_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sed -i "s/error.log/error_$WEBSITE_ROLE_INSTANCE_ID.log/g" /etc/apache2/apache2.conf 
+sed -i "s/access.log/access_$WEBSITE_ROLE_INSTANCE_ID.log/g" /etc/apache2/apache2.conf
+mkdir /var/lock/apache2
+mkdir /var/run/apache2
+mkdir /var/log/apache2
+touch /var/log/apache2/access_$WEBSITE_ROLE_INSTANCE_ID.log
+echo "$(date) Container started" >> /var/log/apache2/access_$WEBSITE_ROLE_INSTANCE_ID.log
+/usr/sbin/apache2ctl -D FOREGROUND
