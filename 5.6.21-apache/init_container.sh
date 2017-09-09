e49e6510d26691cd6194a8aef38f9f3ef7e7601a
naziml <naziml@gmail.com>
2017-09-08 18:24:22 -0700

    Update apache log to stderr and stdout. Update ssh banner. Update ssh start dir

--- 5.6.21-apache/init_container.sh
+++ 5.6.21-apache/init_container.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+cat >/etc/motd <<EOL 
+  _____                               
+  /  _  \ __________ _________   ____  
+ /  /_\  \\___   /  |  \_  __ \_/ __ \ 
+/    |    \/    /|  |  /|  | \/\  ___/ 
+\____|__  /_____ \____/ |__|    \___  >
+        \/      \/                  \/ 
+   W E B   A P P   O N   L I N U X
+
+Documentation: http://aka.ms/webapp-linux
+PHP quickstart: https://aka.ms/php-qs
+
+EOL
+cat /etc/motd
+
 service ssh start
-sed -i "s/error.log/error_$WEBSITE_ROLE_INSTANCE_ID.log/g" /etc/apache2/apache2.conf 
-sed -i "s/access.log/access_$WEBSITE_ROLE_INSTANCE_ID.log/g" /etc/apache2/apache2.conf
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2
 mkdir /var/run/apache2
-mkdir /var/log/apache2
-touch /var/log/apache2/access_$WEBSITE_ROLE_INSTANCE_ID.log
-echo "$(date) Container started" >> /var/log/apache2/access_$WEBSITE_ROLE_INSTANCE_ID.log
 /usr/sbin/apache2ctl -D FOREGROUND
