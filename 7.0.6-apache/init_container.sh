ac423facfd60dde67a3c37f31d0f073b2cabe528
naziml <naziml@gmail.com>
2017-12-05 11:47:06 -0800

    Add env vars to profile so they show up in ssh

--- 7.0.6-apache/init_container.sh
+++ 7.0.6-apache/init_container.sh
@@ -14,6 +14,9 @@ PHP quickstart: https://aka.ms/php-qs
 EOL
 cat /etc/motd
 
+# Get environment variables to show up in SSH session
+eval $(printenv | awk -F= '{print "export " $1"="$2 }' >> /etc/profile)
+
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2
