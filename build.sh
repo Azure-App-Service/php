b9d6770962bdd8273a8f190d266074451d67d4cb
Lukasz Stempniewicz <lukaszcorgi@gmail.com>
2018-02-01 09:06:45 -0800

    Update build.sh to push :latest (#23)

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@ buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 # docker build -t "$1"/php:5.6.21-apache-xdebug_"$buildnumber" 5.6.21-apache-xdebug
 # docker build -t "$1"/php:7.0.6-apache_"$buildnumber" 7.0.6-apache
 # docker build -t "$1"/php:7.0.6-apache-xdebug_"$buildnumber" 7.0.6-apache-xdebug
-# docker build -t "$1"/php:7.2.1-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.1-apache
-docker build -t "$1"/php:7.2.1-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.2.1-apache-xdebug
+#docker build -t "$1"/php:7.2.1-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.2.1-apache-xdebug
+docker build -t "$1"/php:7.2.1-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.1-apache
+docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
 
 docker login -u "$2" -p "$3"
 
@@ -17,6 +18,7 @@ docker login -u "$2" -p "$3"
 # docker push "$1"/php:7.0.6-apache_"$buildnumber"
 # docker push "$1"/php:7.0.6-apache-xdebug_"$buildnumber"
 docker push "$1"/php:latest_"$buildnumber"
+docker push "$1"/php:latest
 docker push "$1"/php:7.2.1-apache_"$buildnumber"
 docker push "$1"/php:latest-xdebug_"$buildnumber"
 docker push "$1"/php:7.2.1-apache-xdebug_"$buildnumber"
