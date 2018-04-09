ab5e168a71530c04690b02502a80086153d43b5e
Lukasz Stempniewicz <lukaszcorgi@gmail.com>
2018-04-09 14:57:57 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ set -x -e
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
-# docker build -t "$1"/php:5.6.21-apache_"$buildnumber" 5.6.21-apache
-# docker build -t "$1"/php:5.6.21-apache-xdebug_"$buildnumber" 5.6.21-apache-xdebug
-# docker build -t "$1"/php:7.0.6-apache_"$buildnumber" 7.0.6-apache
-# docker build -t "$1"/php:7.0.6-apache-xdebug_"$buildnumber" 7.0.6-apache-xdebug
-#docker build -t "$1"/php:7.2.1-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.2.1-apache-xdebug
-docker build -t "$1"/php:7.2.1-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.1-apache
+# docker build --no-cache -t "$1"/php:5.6.21-apache_"$buildnumber" 5.6.21-apache
+# docker build --no-cache -t "$1"/php:5.6.21-apache-xdebug_"$buildnumber" 5.6.21-apache-xdebug
+# docker build --no-cache -t "$1"/php:7.0.6-apache_"$buildnumber" 7.0.6-apache
+# docker build --no-cache -t "$1"/php:7.0.6-apache-xdebug_"$buildnumber" 7.0.6-apache-xdebug
+# docker build --no-cache -t "$1"/php:7.2.1-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.2.1-apache-xdebug
+docker build --no-cache -t "$1"/php:7.2.1-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.1-apache
 docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
 
 docker login -u "$2" -p "$3"
