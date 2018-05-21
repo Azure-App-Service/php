7ebdb9caf9a9e133decc4e31857aeb76fd293211
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2018-05-21 16:02:38 -0700

    use build.sh to build xdebug as well

--- build.sh
+++ build.sh
@@ -7,17 +7,34 @@ set -x -e
 
 buildnumber=${4-$(date -u +"%y%m%d%H%M")}
 
-docker build -q --no-cache -t "$1"/php:5.6.36-apache_"$buildnumber" 5.6.36-apache
-docker build -q --no-cache -t "$1"/php:7.0.30-apache_"$buildnumber" 7.0.30-apache
-docker build -q --no-cache -t "$1"/php:7.2.5-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.5-apache
-docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
-
 docker login -u "$2" -p "$3"
+
+# build base images
+docker build -q -t "$1"/php:5.6.36-apache_"$buildnumber" 5.6.36-apache
+docker build -q -t "$1"/php:7.0.30-apache_"$buildnumber" 7.0.30-apache
+docker build -q -t "$1"/php:7.2.5-apache_"$buildnumber" -t "$1"/php:latest_"$buildnumber" 7.2.5-apache
+docker tag "$1"/php:latest_"$buildnumber" "$1"/php:latest
 
 docker push "$1"/php:5.6.36-apache_"$buildnumber"
 docker push "$1"/php:7.0.30-apache_"$buildnumber"
 docker push "$1"/php:7.2.5-apache_"$buildnumber"
 docker push "$1"/php:latest_"$buildnumber"
 docker push "$1"/php:latest
+
+# xdebug depends on base images
+# generate dockerfile for xdebug
+sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 5.6.36-apache-xdebug/Dockerfile.template > 5.6.36-apache-xdebug/Dockerfile
+sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.0.30-apache-xdebug/Dockerfile.template > 7.0.30-apache-xdebug/Dockerfile
+sed -e s/reponame/"$1"/g -e s/buildnumber/"$buildnumber"/g 7.2.5-apache-xdebug/Dockerfile.template > 7.2.5-apache-xdebug/Dockerfile
+
+# build xdebug images
+docker build -q -t "$1"/php:5.6.36-apache-xdebug_"$buildnumber" 5.6.36-apache-xdebug
+docker build -q -t "$1"/php:7.0.30-apache-xdebug_"$buildnumber" 7.0.30-apache-xdebug
+docker build -q -t "$1"/php:7.2.5-apache-xdebug_"$buildnumber" -t "$1"/php:latest-xdebug_"$buildnumber" 7.2.5-apache-xdebug
+
+docker push "$1"/php:5.6.36-apache-xdebug_"$buildnumber"
+docker push "$1"/php:7.0.30-apache-xdebug_"$buildnumber"
+docker push "$1"/php:7.2.5-apache-xdebug_"$buildnumber"
+docker push "$1"/php:latest-xdebug_"$buildnumber"
 
 docker logout
