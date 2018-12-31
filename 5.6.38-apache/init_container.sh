88c9f187cd4ed0fc437989f9f1cd45e3a09b74b4
Roel Arents <roel.arents@luminis.eu>
2018-12-31 12:02:11 +0100

    add a DOCROOTSUB env var
    to set an optional subdir as the DocumentRoot

--- 5.6.38-apache/init_container.sh
+++ 5.6.38-apache/init_container.sh
@@ -19,6 +19,7 @@ eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/
 
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
+sed -i "s/{DOCROOTSUB}/$DOCROOTSUB/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2
 mkdir /var/run/apache2
 /usr/sbin/apache2ctl -D FOREGROUND
