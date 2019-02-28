c6723a52ce1ac582669e90874f2ddf8c6b388a90
Ranjith Ramachandra <ranjithr@microsoft.com>
2019-02-28 16:35:45 +0000

    Update ports

--- 7.2.11-apache/init_container.sh
+++ 7.2.11-apache/init_container.sh
@@ -17,6 +17,7 @@ cat /etc/motd
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+sed -i "s/{SSH_PORT}/$SSH_PORT/g" /etc/ssh/sshd_config
 service ssh start
 sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
 mkdir /var/lock/apache2
