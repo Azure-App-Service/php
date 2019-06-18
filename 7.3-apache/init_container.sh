eab958d7fb8785d70790fa11d828312c844132d7
Blimp <patle@microsoft.com>
2019-06-17 21:34:29 -0700

    Blimp

--- 7.3-apache/init_container.sh
+++ 7.3-apache/init_container.sh
@@ -17,16 +17,24 @@ cat /etc/motd
 # Get environment variables to show up in SSH session
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+# starting sshd process
 sed -i "s/SSH_PORT/$SSH_PORT/g" /etc/ssh/sshd_config
-service ssh start
-sed -i "s/{PORT}/$PORT/g" /etc/apache2/apache2.conf
-mkdir /var/lock/apache2
-mkdir /var/run/apache2
+/usr/sbin/sshd
 
-echo "$@" > /opt/startup/startupCommand
-/opt/startup/generateStartupCommand.sh
-chmod 755 /opt/startup/startupCommand
+appPath="/home/site/wwwroot"
+runFromPath="/tmp/webapp"
+startupCommandPath="/opt/startup/startup.sh"
+userStartupCommand="$@"
+if [ -z "$userStartupCommand" ]
+then
+  userStartupCommand="apache2-foreground";
+else
+  userStartupCommand="$userStartupCommand; apache2-foreground;"
+fi
 
-STARTUPCOMMAND=$(cat /opt/startup/startupCommand)
-echo "Running $STARTUPCOMMAND"
-eval "exec $STARTUPCOMMAND"
+oryxArgs="-appPath $appPath -output $startupCommandPath \
+    -bindPort $PORT -startupCommand '$userStartupCommand'"
+
+echo "Running oryx $oryxArgs"
+eval oryx $oryxArgs
+$startupCommandPath
