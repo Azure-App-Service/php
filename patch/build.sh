f57534d81a29b30b7c947f1ac2f21f695a39b036
Patrick Lee <31744877+patricklee2@users.noreply.github.com>
2019-07-11 16:35:14 -0700

    patch

---
+++ patch/build.sh
@@ -0,0 +1,10 @@
+docker build -t appsvctest/php:7.3.2-apache_1903071319-2  7.3
+docker build -t appsvctest/php:7.2.15-apache_1903071319-2 7.2
+docker build -t appsvctest/php:7.0.33-apache_1903071319-2 7.0
+docker build -t appsvctest/php:5.6.40-apache_1903071319-2 5.6
+
+docker build -t appsvctest/php:7.3.2-apache-xdebug_1903071319-2  7.3-xdebug
+docker build -t appsvctest/php:7.2.15-apache-xdebug_1903071319-2 7.2-xdebug
+docker build -t appsvctest/php:7.0.33-apache-xdebug_1903071319-2 7.0-xdebug
+docker build -t appsvctest/php:5.6.40-apache-xdebug_1903071319-2 5.6-xdebug
+
